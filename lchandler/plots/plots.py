6613f7fac59321b9bcb57c18374b315cd86f9ab1
oscar pimentel <oscarlo.pimentel@gmail.com>
2020-11-05 13:12:27 -0300

    pre-tesis2

--- lchandler/plots/plots.py
+++ lchandler/plots/plots.py
@@ -58,6 +58,7 @@ def plot_obse_samplers(lcdataset, set_name, obse_sampler_bdict,
 	original_space:bool=1,
 	pdf_scale:float=0.01,
 	figsize:tuple=(12,8),
+	add_samples=0,
 	):
 	fig, axs = plt.subplots(1, 2, figsize=figsize)
 	band_names = obse_sampler_bdict.keys()
@@ -65,44 +66,57 @@ def plot_obse_samplers(lcdataset, set_name, obse_sampler_bdict,
 	for kb,b in enumerate(band_names):
 		ax = axs[kb]
 		obse_sampler = obse_sampler_bdict[b]
-		colors = cm.viridis(np.linspace(0, 1, len(obse_sampler.distrs)))
-		for p_idx in range(len(obse_sampler.distrs)):
-			d = obse_sampler.distrs[p_idx]
-			obse = obse_sampler.raw_obse[obse_sampler.obs_indexs_per_range[p_idx]] if original_space else d['scaler'].transform(-obse_sampler.raw_obse[obse_sampler.obs_indexs_per_range[p_idx]][:,None])[:,0]
-			obs = obse_sampler.raw_obs[obse_sampler.obs_indexs_per_range[p_idx]]
-			
-			if p_idx%3==0:
-				self_x =  obse_sampler.obse if original_space else d['scaler'].transform(-obse_sampler.obse[:,None])[:,0]
-				ls_x_pdf = np.linspace(self_x.min(), self_x.max(), 100)
-				tx_pdf = d['scaler'].transform(-ls_x_pdf[:,None])[:,0] if original_space else ls_x_pdf
-				pdf = d['distr'].pdf(tx_pdf, *d['params'])
-				rank_ranges = obse_sampler.rank_ranges[p_idx]
-				#for k in range(len(ls_x_pdf)-1):
-				#    alpha = pdf[k]/pdf.max()
-				#    ax.fill_between([ls_x_pdf[k], ls_x_pdf[k+1]], rank_ranges[0], rank_ranges[1], fc='r', alpha=alpha)
-				
-				#pdf_offset = rank_ranges[0]+(rank_ranges[1]-rank_ranges[0])/2 # middle of rank range
-				pdf_offset = rank_ranges[1] # upper of rank range
-				pdf = pdf/pdf.max()*pdf_scale+pdf_offset
-				alpha = 1-p_idx/len(obse_sampler.distrs)
-				alpha = 1
-				c = colors[p_idx]
-				ax.plot(ls_x_pdf, pdf, c=c, alpha=alpha, lw=1, label='$P(\sigma_x|x)$ beta fit' if p_idx==0 else None)
-				
-			ax.plot(obse, obs, 'k.', markersize=2, alpha=0.2)
-		ax.plot(np.nan, np.nan, 'k.', alpha=1, label='original samples')
+		if original_space:
+			label='original samples $\sim p(\sigma_x,x)$'
+			ax.plot(obse_sampler.raw_obse, obse_sampler.raw_obs, 'k.', markersize=2, alpha=0.2); ax.plot(np.nan, np.nan, 'k.', label=label)
+			x = np.linspace(obse_sampler.raw_obse.min(), obse_sampler.raw_obse.max(), 100)
+			ax.plot(x, x*obse_sampler.m+obse_sampler.n, 'b', alpha=0.75, label='rotation axis', lw=1)
+			ax.plot(obse_sampler.lr_x, obse_sampler.lr_y, 'b.', alpha=1, markersize=4); ax.plot(np.nan, np.nan, 'b.', label='rotation axis support samples')
 
-		#ax.plot(obse_sampler.lr_x, obse_sampler.lr_y, 'ro', alpha=1, label='s', markersize=4)
-		x = np.linspace(obse_sampler.raw_obse.min(), obse_sampler.raw_obse.max(), 100)
-		ax.plot(x, x*obse_sampler.m+obse_sampler.n, 'r', alpha=1, label='outliers threshold dropout', lw=1)
+			### add samples
+			if add_samples:
+				n = int(1e3)
+				to_sample = obse_sampler.raw_obs
+				std = 1e-4
+				new_obs = [np.random.normal(to_sample[np.random.randint(0, len(to_sample))], std) for _ in range(n)] # kde
+				#x = 0.05; new_obs = np.linspace(x, x+0.001, 1000) # sanity check
+				new_obse, new_obs = obse_sampler.conditional_sample(new_obs)
+				ax.plot(new_obse, new_obs, 'r.', markersize=2, alpha=1); ax.plot(np.nan, np.nan, 'r.', label='synthetic samples $\sim \hat{p}(\sigma_x,x)$')
+
+		else:
+			label='original samples $\sim p(\sigma_x'+"'"+',x'+"'"+')$'
+			ax.plot(obse_sampler.obse, obse_sampler.obs, 'k.', markersize=2, alpha=0.2); ax.plot(np.nan, np.nan, 'k.', label=label)
+			min_obse = obse_sampler.obse.min()
+			max_obse = obse_sampler.obse.max()
+			pdfx = np.linspace(min_obse, max_obse, 200)
+			colors = cm.viridis(np.linspace(0, 1, len(obse_sampler.distrs)))
+			for p_idx in range(len(obse_sampler.distrs)):
+				d = obse_sampler.distrs[p_idx]
 				
-		title = f'survey:{lcset.survey} - set: {set_name} - band: {b}'
+				if p_idx%5==0:
+					rank_ranges = obse_sampler.rank_ranges[p_idx]
+					pdf_offset = rank_ranges[1] # upper of rank range
+					pdfy = d['distr'].pdf(pdfx, *d['params'])
+					pdfy = pdfy/pdfy.max()*pdf_scale+pdf_offset
+					c = colors[p_idx]
+					label = '$\hat{p}(\sigma_x'+"'"+'|x'+"'"+')$ Gamma fit'
+					ax.plot(pdfx, pdfy, c=c, alpha=1, lw=1, label=label if p_idx==0 else None)
+				
+		if original_space:
+			title = f'survey:{lcset.survey} - set: {set_name} - band: {b}'
+			ax.set_xlabel('obs-error')
+			ax.set_ylabel('obs' if kb==0 else None)
+			ax.set_xlim([0.0, 0.02])
+			ax.set_ylim([0.0, 0.25])
+		else:
+			title = f'survey:{lcset.survey} - set: {set_name} - band: {b}'
+			ax.set_xlabel('rotated-flipped obs-error')
+			ax.set_ylabel('rotated obs' if kb==0 else None)
+			ax.set_xlim([0.0, 0.02])
+			ax.set_ylim([0.0, 0.25])
+
 		ax.set_title(title)
-		ax.set_xlabel('obs error')
-		ax.set_ylabel('obs' if kb==0 else None)
-		ax.legend()
-		#ax.set_xlim([0.0025, 0.02])
-		ax.set_ylim([0, 0.4])
+		ax.legend(loc='lower right')
 
 		### multiband colors
 		#ax.grid(color=C_.COLOR_DICT[b])
--- lchandler/synthetic/distr_fitting.py
+++ lchandler/synthetic/distr_fitting.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 from __future__ import division
 from . import C_
 
+import math
 import numpy as np
 import scipy.stats as stats
 from sklearn import preprocessing as prep
@@ -9,14 +10,47 @@ from flamingchoripan.datascience.statistics import dropout_extreme_percentiles, 
 
 ###################################################################################################################################################
 
+class CustomRotor():
+	def __init__(self, m, n):
+		self.m = m
+		assert math.atan(m)>0 and math.atan(m)<np.pi/2
+		self.a = np.pi/2-math.atan(m)
+		self.n = n
+		self.rot = np.linalg.inv(np.array([[np.cos(self.a), np.sin(self.a)], [np.cos(self.a+np.pi/2), np.sin(self.a+np.pi/2)]]))
+		self.inv_rot = np.linalg.inv(self.rot)
+
+	def transform(self, obse, obs):
+		x = np.concatenate([obse[:,None], obs[:,None]], axis=-1)
+		assert x.shape[-1]==2
+		x[:,1] = x[:,1]-self.n
+		x = (self.rot@x.T).T
+		x[:,0] = -1*x[:,0]
+		return x[:,0], x[:,1]
+
+	def inverse_transform(self, obse, obs):
+		x = np.concatenate([obse[:,None], obs[:,None]], axis=-1)
+		assert x.shape[-1]==2
+		x[:,0] = -1*x[:,0]
+		x = (self.inv_rot@x.T).T
+		x[:,1] = x[:,1]+self.n
+		return x[:,0], x[:,1]
+
+###################################################################################################################################################
+
 class ObsErrorConditionalSampler():
 	def __init__(self, lcdataset:dict, set_name:str, b:str,
-		samples_per_range:int=15,
+		samples_per_range:int=50,
+		rank_threshold=0.04,
+		dist_threshold=5e-4,
+		neighborhood_n=10,
 		):
 		self.lcdataset = lcdataset
 		self.lcset = lcdataset[set_name]
 		self.b = b
 		self.samples_per_range = samples_per_range
+		self.rank_threshold = rank_threshold
+		self.dist_threshold = dist_threshold
+		self.neighborhood_n = neighborhood_n
 		self.raw_obse = np.concatenate([lcobj.get_b(b).obse for lcobj in self.lcset.get_lcobjs()])
 		self.raw_obs = np.concatenate([lcobj.get_b(b).obs for lcobj in self.lcset.get_lcobjs()])
 		self.min_obs = self.raw_obs.min()
@@ -24,21 +58,39 @@ class ObsErrorConditionalSampler():
 		self.reset()
 		
 	def get_m_n(self):
-		obse = self.raw_obse
-		obs = self.raw_obs
-		n = 6
-		grid = np.linspace(0, 0.15, n+1)
-		index_per_range = [np.where((obs>grid[k]) & (obs<=grid[k+1])) for k in range(len(grid)-1)]
+		obse = []
+		obs = []
+		for k1 in range(len(self.raw_obse)):
+			obse1 = self.raw_obse[k1]
+			obs1 = self.raw_obs[k1]
+			if obs1>self.rank_threshold:
+				continue
+			neighborhood = 0
+			for k2 in range(len(self.raw_obse)):
+				if k1==k2:
+					continue
+				obse2 = self.raw_obse[k2]
+				obs2 = self.raw_obs[k2]
+				dist = np.linalg.norm(np.array([obse1-obse2, obs1-obs2]))
+				if dist<=self.dist_threshold:
+					neighborhood += 1
+					if neighborhood>=self.neighborhood_n:
+						obse.append(obse1)
+						obs.append(obs1)
+						break
+
+		assert len(obse)>0
+		obse = np.array(obse)
+		obs = np.array(obs)
+		rank_ranges, index_per_range, ranks = get_linspace_ranks(obs, self.samples_per_range)
 
 		self.lr_x = []
 		self.lr_y = []
 		for k,indexs in enumerate(index_per_range):
-			if len(indexs[0])<=1:
+			if len(indexs[0])==0:
 				continue
 			sub_obse = obse[indexs]
 			sub_obs = obs[indexs]
-			sub_obse, ind = dropout_extreme_percentiles(sub_obse, p=np.exp(-k*2)*15, mode='upper')
-			sub_obs = sub_obs[ind]
 			i = sub_obse.argmax()
 			self.lr_x.append(sub_obse[i])
 			self.lr_y.append(sub_obs[i])
@@ -47,58 +99,51 @@ class ObsErrorConditionalSampler():
 		slope, intercept, r_value, p_value, std_err = stats.linregress(self.lr_x, self.lr_y)
 		self.m = slope
 		self.n = intercept
+		self.rotor = CustomRotor(self.m, self.n, )
 
 	def reset(self):
 		### fit diagonal line
 		self.get_m_n()
-		valid_indexs = np.where(self.raw_obs>=self.raw_obse*self.m+self.n)
-		self.obse = self.raw_obse[valid_indexs]
-		self.obs = self.raw_obs[valid_indexs]
+
+		### rotate space & clip
+		p = 1
+		self.obse, self.obs = self.rotor.transform(self.raw_obse, self.raw_obs)
+		valid_indexs = np.where(
+			(self.obse>=0) &
+			(self.obs>np.percentile(self.obs, p)) & (self.obs<np.percentile(self.obs, 100-p))
+		)
+		self.obse = self.obse[valid_indexs]
+		self.obs = self.obs[valid_indexs]
 
 		### generate obs grid
 		self.rank_ranges, self.obs_indexs_per_range, self.ranks = get_linspace_ranks(self.obs, self.samples_per_range)
 		self.distrs = [self.get_fitted_distr(obs_indexs, k) for k,obs_indexs in enumerate(self.obs_indexs_per_range)]
 		
-	def get_fitted_distr(self, obs_indexs, k,
-		eps=1e-3,
-		):
-		#distr_name = 'norm'
-		#distr_name = 'truncnorm'
-		distr_name = 'skewnorm'
-		#distr_name = 'beta'
-		#distr_name = 't'
-		distr = getattr(stats, distr_name)
-
-		## clean by percentile
-		p = np.exp(-k*2)*6
-		obse_values,_ = dropout_extreme_percentiles(self.obse[obs_indexs], p, mode='upper')
-		
-		scaler_kwargs = {
-			'n_quantiles':min(1000, len(obse_values)),
-			'output_distribution':'normal',
-		}
-		#scaler = prep.QuantileTransformer(**scaler_kwargs)
-		scaler = prep.MinMaxScaler(feature_range=(0+eps, 1-eps))
-		#scaler = prep.StandardScaler()
-		
-		obse_values = scaler.fit_transform(-obse_values[:,None])[:,0]
-		if distr_name=='beta':
-			params = distr.fit(obse_values, floc=0, fscale=1)
-		else:
-			params = distr.fit(obse_values)
-		return {'distr':distr, 'params':params, 'scaler':scaler}
+	def get_fitted_distr(self, obs_indexs, k):
+		### clean by percentile
+		#p = np.exp(-k*2)*6
+		p = 5
+		obse_values,_ = dropout_extreme_percentiles(self.obse[obs_indexs], p, mode='both')
+		distr = getattr(stats, 'gamma')
+		params = distr.fit(obse_values, floc=0)
+		return {'distr':distr, 'params':params}
 	
 	def get_percentile_range(self, obs):
 		return np.where(np.clip(obs, None, self.obs.max())<=self.rank_ranges[:,1])[0][0]
 		
-	def conditional_sample_i(self, obs_):
-		d = self.distrs[self.get_percentile_range(obs_)]
-		samples = d['distr'].rvs(*d['params'], size=1)
-		samples = -d['scaler'].inverse_transform(samples[:,None])[:,0]
-		return samples
+	def conditional_sample_i(self, obs):
+		#new_obs = np.clip(obs, self.min_obs, None) # can't
+		new_obse = np.array([0])
+		new_obs = np.array([obs])
+		new_obse, _ = self.rotor.inverse_transform(new_obse, new_obs)
+		d = self.distrs[self.get_percentile_range(new_obs)]
+		new_obse = d['distr'].rvs(*d['params'], size=1)
+		new_obse, _ = self.rotor.inverse_transform(new_obse, new_obs)
+		return new_obse, new_obs
 		
 	def conditional_sample(self, obs):
-		return np.array([self.conditional_sample_i(obs_) for obs_ in obs])[:,0]
+		x = np.array([self.conditional_sample_i(obs_) for obs_ in obs])
+		return x[:,0], x[:,1]
 
 ###################################################################################################################################################
 
