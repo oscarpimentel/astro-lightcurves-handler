968eb0bf8e8670225197f0600889da42fd8eba05
opimentel-tesla <oscarlo.pimentel@gmail.com>
2021-05-08 10:01:06 -0400

    tesla

--- experiments/export_images.py
+++ experiments/export_images.py
@@ -10,48 +10,51 @@ if __name__== '__main__':
 	from flamingchoripan.prints import print_big_bar
 
 	parser = argparse.ArgumentParser('usage description')
-	parser.add_argument('-method',  type=str, default='spm-mcmc-fstw', help='method')
+	parser.add_argument('-method',  type=str, default='.', help='method')
 	main_args = parser.parse_args()
 	print_big_bar()
 
 	###################################################################################################################################################
 	import numpy as np
-	from flamingchoripan.files import load_pickle, save_pickle, get_dict_from_filedir
-
-	filedir = f'../../surveys-save/survey=alerceZTFv7.1~bands=gr~mode=onlySNe~method={main_args.method}.splcds'
-	filedict = get_dict_from_filedir(filedir)
-	rootdir = filedict['_rootdir']
-	cfilename = filedict['_cfilename']
-	survey = filedict['survey']
-	lcdataset = load_pickle(filedir)
-	print(lcdataset)
-
-	###################################################################################################################################################
 	import flamingchoripan.lists as lists
+	from flamingchoripan.files import load_pickle, save_pickle, get_dict_from_filedir
 	from flamingchoripan.cuteplots.utils import save_fig
 	import matplotlib.pyplot as plt
 	from lchandler.plots.lc import plot_lightcurve
 	from flamingchoripan.files import save_time_stamp
 
-	figsize = (12,5)
-	lcset_names = lcdataset.get_lcset_names()
-	for lcset_name in lcset_names:
-		lcset = lcdataset[lcset_name]
-		for lcobj_name in lcset.get_lcobj_names():
-			fig, ax = plt.subplots(1,1, figsize=figsize)
-			lcobj = lcset[lcobj_name]
-			c = lcset.class_names[lcobj.y]
-			for kb,b in enumerate(lcset.band_names):
-				plot_lightcurve(ax, lcobj, b, label=f'{b} obs')
-			title = f'survey={lcset.survey}-{"".join(lcset.band_names)} - obj={lcobj_name} [{lcset.class_names[lcobj.y]}]'
-			ax.set_title(title)
-			ax.set_xlabel('time [days]')
-			ax.set_ylabel('observation [flux]')
-			ax.grid(alpha=0.5)
-			ax.legend()
-			save_filedir = f'../save/{cfilename}/{lcset_name}/{c}/{lcobj_name}.png'
-			save_fig(save_filedir, fig)
-			save_time_stamp(f'../save/{cfilename}')
-			#break
+	methods = ['linear-fstw', 'bspline-fstw', 'spm-mle-fstw', 'spm-mle-estw', 'spm-mcmc-fstw', 'spm-mcmc-estw'] if main_args.method=='.' else main_args.method
+	methods = [methods] if isinstance(methods, str) else methods
+	
+	for method in methods:
+		filedir = f'../../surveys-save/survey=alerceZTFv7.1~bands=gr~mode=onlySNe~method={method}.splcds'
+		filedict = get_dict_from_filedir(filedir)
+		rootdir = filedict['_rootdir']
+		cfilename = filedict['_cfilename']
+		lcdataset = load_pickle(filedir)
+		lcset_info = lcdataset['raw'].get_info()
+		print(lcdataset)
+		
+		lcset_names = lcdataset.get_lcset_names()
+		for lcset_name in lcset_names:
+			lcset = lcdataset[lcset_name]
+			for lcobj_name in lcset.get_lcobj_names():
+				print(f'method={method} - lcset_name={lcset_name} - lcobj_name={lcobj_name}')
+				figsize = (12,5)
+				fig, ax = plt.subplots(1,1, figsize=figsize)
+				lcobj = lcset[lcobj_name]
+				c = lcset.class_names[lcobj.y]
+				for kb,b in enumerate(lcset.band_names):
+					plot_lightcurve(ax, lcobj, b, label=f'{b} obs')
+				title = f'survey={lcset.survey}-{"".join(lcset.band_names)} [{lcset_name}] - obj={lcobj_name} [{lcset.class_names[lcobj.y]}]'
+				ax.set_title(title)
+				ax.set_xlabel('time [days]')
+				ax.set_ylabel('observation [flux]')
+				ax.grid(alpha=0.5)
+				ax.legend()
+				save_filedir = f'../save/{cfilename}/{lcset_name}/{c}/{lcobj_name}.png'
+				save_fig(save_filedir, fig)
+				save_time_stamp(f'../save/{cfilename}')
+				#break
 
 
