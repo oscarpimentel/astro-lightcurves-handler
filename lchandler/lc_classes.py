f55b7ec458b89e59b0aa4242cca86d28b72e68b4
opimentel-tesla <oscarlo.pimentel@gmail.com>
2021-05-25 11:06:22 -0400

    torch

--- lchandler/lc_classes.py
+++ lchandler/lc_classes.py
@@ -6,22 +6,29 @@ from numba import jit
 import numpy as np
 import random
 from scipy.stats import t
-import fuzzytools.numba as fcnumba
+#import fuzzytools.numba as torch
 from copy import copy, deepcopy
+import torch
 
 ###################################################################################################################################################
 
-def diff_vector(x:np.ndarray,
+def diff_vector(x,
 	uses_prepend=True,
 	):
-	return fcnumba.diff_vector(x, uses_prepend)
+	if uses_prepend:
+		new_x = torch.cat([x[0][None,...], x], dim=0)
+	else:
+		new_x = x
+	dx = new_x[1:]-new_x[:-1]
+	return dx
 
 def get_obs_noise_gaussian(obs, obse, obs_min_lim,
 	std_scale:float=C_.OBSE_STD_SCALE,
 	mode='norm',
 	):
 	if mode=='norm':
-		obs_values = np.clip(fcnumba.normal(obs, obse*std_scale), obs_min_lim, None)
+		obs_values = torch.normal(obs, obse*std_scale)
+		obs_values = torch.clip(obs_values, obs_min_lim, None)
 	else:
 		raise Exception(f'no mode {mode}')
 	return obs_values
@@ -32,7 +39,7 @@ class SubLCO():
 	'''
 	Dataclass object used to store an astronomical light curve
 	'''
-	def __init__(self, days:np.ndarray, obs:np.ndarray, obs_errors:np.ndarray,
+	def __init__(self, days, obs, obs_errors,
 		y:int=None,
 		):
 		self.set_values(days, obs, obs_errors)
@@ -40,7 +47,11 @@ class SubLCO():
 		self.reset()
 
 	def reset(self):
-		self.set_synthetic_mode(None)
+		self.set_device('cpu')
+		self.set_synthetic_mode(None)	
+
+	def set_device(self, device):
+		self.device = device
 
 	def get_synthetic_mode(self):
 		return self.synthetic_mode
@@ -51,29 +62,37 @@ class SubLCO():
 	def is_synthetic(self):
 		return not self.synthetic_mode is None
 
-	def set_values(self, days:np.ndarray, obs:np.ndarray, obs_errors:np.ndarray):
+	def set_values(self, days, obs, obs_errors):
 		'''
 		Always use this method to set new values!
 		'''
 		assert len(days)==len(obs)
 		assert len(obs)==len(obs_errors)
-		self.set_days(days)
-		self.set_obs(obs)
-		self.set_obse(obs_errors)
+		if isinstance(days, torch.Tensor):
+			tdays = days.clone().detach()
+			tobs = obs.clone().detach()
+			tobse = obs_errors.clone().detach()
+		else:
+			tdays = torch.Tensor(days)
+			tobs = torch.Tensor(obs)
+			tobse = torch.Tensor(obs_errors)
+		self.set_days(tdays)
+		self.set_obs(tobs)
+		self.set_obse(tobse)
 
 	def set_days(self, days):
 		assert len(days.shape)==1
-		assert np.all((diff_vector(days, uses_prepend=False)>0)) # check if days are in order
+		assert torch.all((diff_vector(days, uses_prepend=False)>0)) # check if days are in order
 		self.days = days
 
 	def set_obs(self, obs):
 		assert len(obs.shape)==1
-		assert np.all(obs>=0)
+		assert torch.all(obs>=0)
 		self.obs = obs
 
 	def set_obse(self, obs_errors):
 		assert len(obs_errors.shape)==1
-		if not np.all(obs_errors>0):
+		if not torch.all(obs_errors>0):
 			raise Exception(f'wrong obs_errors: {obs_errors}')
 		self.obse = obs_errors
 
@@ -87,7 +106,7 @@ class SubLCO():
 		'''
 		assert len(self)==len(values)
 		new_days = self.days+values
-		valid_indexs = fcnumba.argsort(new_days) # must sort before the values to mantain sequenciality
+		valid_indexs = torch.argsort(new_days) # must sort before the values to mantain sequenciality
 		self.days = new_days # bypass set_days() because non-sorted asumption
 		self.apply_valid_indexs_to_attrs(valid_indexs, recalculate) # apply valid indexs to all
 
@@ -99,7 +118,8 @@ class SubLCO():
 		'''
 		if hours_noise==0:
 			return
-		hours_noise = fcnumba.uniform(-hours_noise, hours_noise, len(self))
+
+		hours_noise = torch.FloatTensor(len(self)).uniform_(-hours_noise, hours_noise)
 		self.add_day_values(hours_noise/24., recalculate)
 
 	def add_obs_values(self, values,
@@ -129,7 +149,7 @@ class SubLCO():
 		'''
 		if std_scale==0:
 			return
-		assert np.all(self.obs>=obs_min_lim)
+		assert torch.all(self.obs>=obs_min_lim)
 		obs_values = get_obs_noise_gaussian(self.obs, self.obse, obs_min_lim, std_scale, mode)
 		self.add_obs_values(obs_values-self.obs, recalculate)
 		return
@@ -151,11 +171,12 @@ class SubLCO():
 		if apply_prob<=random.random():
 			return success # exit
 
-		valid_mask = fcnumba.bernoulli(1-ds_prob, len(self))
+		p = torch.full((len(self),), fill_value=1-ds_prob, device=self.device)
+		valid_mask = torch.bernoulli(p).bool()
 		if valid_mask.sum()<min_valid_length: # extra case. If by change the mask implies a very short curve
-			valid_mask = np.zeros((len(self)), dtype=np.bool)
+			valid_mask = torch.zeros((len(self)), dtype=torch.bool)
 			valid_mask[:min_valid_length] = True
-			valid_mask = np.random.permutation(valid_mask)
+			valid_mask = valid_mask[torch.randperm(len(valid_mask))]
 
 		### calcule again as the original values changed
 		self.apply_valid_indexs_to_attrs(valid_mask, recalculate)
@@ -188,12 +209,12 @@ class SubLCO():
 
 		elif mode=='left':
 			new_length = random.randint(min_valid_length, len(self)) # [a,b]
-			valid_mask = np.zeros((len(self)), dtype=np.bool)
+			valid_mask = torch.zeros((len(self)), dtype=torch.bool)
 			valid_mask[:new_length] = True
 
 		elif mode=='random':
 			new_length = random.randint(min_valid_length, len(self)) # [a,b]
-			valid_mask = np.zeros((len(self)), dtype=np.bool)
+			valid_mask = torch.zeros((len(self)), dtype=torch.bool)
 			index = random.randint(0, len(self)-new_length) # [a,b]
 			valid_mask[index:index+new_length] = True
 
@@ -227,7 +248,7 @@ class SubLCO():
 		original_len = len(self)
 		for key in self.__dict__.keys():
 			x = self.__dict__[key]
-			if isinstance(x, np.ndarray): # apply same mask to all np.ndarray in the object
+			if isinstance(x, torch.Tensor): # apply same mask to all in the object
 				assert original_len==len(x)
 				assert len(x.shape)==1 # 1D tensor
 				#print(key)
@@ -273,7 +294,7 @@ class SubLCO():
 
 	def get_custom_x(self, attrs:list):
 		values = [self.get_attr(attr)[...,None] for attr in attrs]
-		x = fcnumba.concatenate(values, axis=-1)
+		x = torch.concatenate(values, dim=-1)
 		return x
 
 	def get_first_day(self):
@@ -293,9 +314,9 @@ class SubLCO():
 
 	def __copy__(self):
 		new_sublco = SubLCO(
-			fcnumba.copy(self.days),
-			fcnumba.copy(self.obs),
-			fcnumba.copy(self.obse),
+			copy(self.days),
+			copy(self.obs),
+			copy(self.obse),
 			self.y,
 			)
 		new_sublco.set_synthetic_mode(self.get_synthetic_mode())
@@ -304,8 +325,8 @@ class SubLCO():
 			if key in ['days', 'obs', 'obse']:
 				continue
 			v = self.__dict__[key]
-			if isinstance(v, np.ndarray):
-				setattr(new_sublco, key, fcnumba.copy(v))
+			if isinstance(v, torch.Tensor):
+				setattr(new_sublco, key, copy(v))
 		return new_sublco
 
 	def __len__(self):
@@ -325,7 +346,7 @@ class SubLCO():
 		i = 0
 		while i<len(self.days):
 			day = self.days[i]
-			valid_indexs = np.where((self.days>=day) & (self.days<day+dt))[0]
+			valid_indexs = torch.where((self.days>=day) & (self.days<day+dt))[0]
 			ddict[day] = valid_indexs
 			i += len(valid_indexs)
 
@@ -334,34 +355,34 @@ class SubLCO():
 		new_obse = []
 		for k in ddict.keys():
 			if mode=='mean':
-				new_days.append(np.mean(self.days[ddict[k]]))
-				new_obs.append(np.mean(self.obs[ddict[k]]))
-				new_obse.append(np.mean(self.obse[ddict[k]]))
+				new_days.append(torch.mean(self.days[ddict[k]]))
+				new_obs.append(torch.mean(self.obs[ddict[k]]))
+				new_obse.append(torch.mean(self.obse[ddict[k]]))
 			elif mode=='min_obse':
-				i = fcnumba.argmin(self.obse[ddict[k]])
+				i = torch.argmin(self.obse[ddict[k]])
 				new_days.append(self.days[ddict[k]][i])
 				new_obs.append(self.obs[ddict[k]][i])
 				new_obse.append(self.obse[ddict[k]][i])
 			elif mode=='expectation':
-				obse_exp = np.exp(-np.log(self.obse[ddict[k]]+C_.EPS))
-				assert len(np.where(obse_exp==np.infty)[0])==0
+				obse_exp = torch.exp(-torch.log(self.obse[ddict[k]]+C_.EPS))
+				assert len(torch.where(obse_exp==torch.infty)[0])==0
 				#print(obse_exp, obse_exp.shape)
 				dist = obse_exp/obse_exp.sum()
-				new_days.append(np.sum(self.days[ddict[k]]*dist))
-				new_obs.append(np.sum(self.obs[ddict[k]]*dist))
-				new_obse.append(np.sum(self.obse[ddict[k]]*dist))
+				new_days.append(torch.sum(self.days[ddict[k]]*dist))
+				new_obs.append(torch.sum(self.obs[ddict[k]]*dist))
+				new_obse.append(torch.sum(self.obse[ddict[k]]*dist))
 			else:
 				raise Exception(f'no mode {mode}')
 
-		self.set_values(np.array(new_days), np.array(new_obs), np.array(new_obse))
+		self.set_values(torch.array(new_days), torch.array(new_obs), torch.array(new_obse))
 
 	def get_snr(self):
 		if len(self)==0:
-			return -np.infty
+			return -torch.infty
 		else:
 			eps = 0
 			snr = (self.obs**2)/(self.obse**2+eps)
-			return np.mean(snr)
+			return torch.mean(snr)
 
 	def __add__(self, other):
 		if other==0 or other is None:
@@ -369,10 +390,10 @@ class SubLCO():
 		elif self==0 or self is None:
 			return copy(other)
 		else:
-			new_days = fcnumba.concatenate([self.days, other.days])
-			new_obs = np.concatenate([self.obs, other.obs])
-			new_obse = np.concatenate([self.obse, other.obse])
-			valid_indexs = fcnumba.argsort(new_days)
+			new_days = torch.cat([self.days, other.days], dim=0)
+			new_obs = torch.cat([self.obs, other.obs], dim=0)
+			new_obse = torch.cat([self.obse, other.obse], dim=0)
+			valid_indexs = torch.argsort(new_days)
 			new_lco = SubLCO(
 				new_days[valid_indexs],
 				new_obs[valid_indexs],
@@ -383,6 +404,13 @@ class SubLCO():
 
 	def __radd__(self, other):
 		return self+other
+
+	def to(self, device):
+		self.set_device(device)
+		for key in self.__dict__.keys():
+			x = self.__dict__[key]
+			if isinstance(x, torch.Tensor): # apply same mask to all in the object
+				self.x = self.x.to(self.device)
 
 ###################################################################################################################################################
 
@@ -409,7 +437,7 @@ class LCO():
 	def reset(self):
 		self.bands = []
 
-	def add_b(self, b:str, days:np.ndarray, obs:np.ndarray, obs_errors:np.ndarray):
+	def add_b(self, b:str, days, obs, obs_errors):
 		'''
 		Always use this method
 		'''
@@ -482,7 +510,7 @@ class LCO():
 		assert len(first_days)>0
 		day_offset = min(first_days) # select the min along all bands
 		for b in bands:
-			self.get_b(b).days -= day_offset
+			self.get_b(b).days = self.get_b(b).days-day_offset
 		if store_day_offset:
 			self.global_first_day = day_offset
 		if return_day_offset:
@@ -494,15 +522,15 @@ class LCO():
 		):
 		bands = self.bands if bands is None else bands
 		values = [self.get_b(b).days for b in bands]
-		all_days = fcnumba.concatenate(values, axis=0)
-		sorted_days_indexs = fcnumba.argsort(all_days)
+		all_days = torch.concatenate(values, axis=0)
+		sorted_days_indexs = torch.argsort(all_days)
 		return sorted_days_indexs
 
 	def get_onehot_serial(self,
 		bands=None,
 		):
 		bands = self.bands if bands is None else bands
-		onehot = np.zeros((len(self), len(bands)), dtype=np.bool)
+		onehot = torch.zeros((len(self), len(bands)), dtype=torch.bool)
 		index = 0
 		for kb,b in enumerate(bands):
 			l = len(getattr(self, b))
@@ -517,7 +545,7 @@ class LCO():
 		):
 		bands = self.bands if bands is None else bands
 		values = [self.get_b(b).get_custom_x(attrs) for b in bands]
-		x = fcnumba.concatenate(values, axis=0)
+		x = torch.concatenate(values, axis=0)
 		sorted_days_indexs = self.get_sorted_days_indexs_serial(bands)
 		x = x[sorted_days_indexs]
 		return x
@@ -545,8 +573,9 @@ class LCO():
 		Duration in days of complete light curve
 		'''
 		bands = self.bands if bands is None else bands
-		days = fcnumba.concatenate([self.get_b(b).days for b in bands], axis=0)
-		return fcnumba.max(days)-fcnumba.min(days)
+		days = torch.cat([self.get_b(b).days for b in bands], dim=0)
+		duration = (torch.max(days)-torch.min(days)).detach().item()
+		return duration
 
 	def set_diff_b(self, b:str, attr:str):
 		self.get_b(b).set_diff(attr) 
@@ -598,5 +627,8 @@ class LCO():
 			self.get_b(b).clean_small_cadence(dt, mode)
 
 	def get_snr(self):
-		return fcnumba.max(np.array([self.get_b(b).get_snr() for b in self.bands]))
+		return torch.max(torch.array([self.get_b(b).get_snr() for b in self.bands]))
 
+	def to(self, device):
+		for b in self.bands:
+			self.get_b().to(device)
